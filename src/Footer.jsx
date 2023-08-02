23b1e59440295af5faee6da09f256413ec490821
Alfonso1025 <alfonso25elorriaga@gmail.com>
2023-08-02 12:09:13 -0500

    modified flex: grow-1 property in message in chatbot component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,6 +4,8 @@ function Footer(){
 
     return(
         <div className="footer-wrapper">
+            <p className="footer-text">Phone: (773) 991 7798</p>
+            <p className="footer-text">Email: alfonso25elorriaga@gmail.com</p>
             <p className="footer-text">Copyright © 2023 Alfonso SoftTech. All rights reserved.</p>
         </div>
     )
